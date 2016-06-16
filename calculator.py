44a5be45704fe39468eda1a68e6004d4911162f0
Seonghun Kang <lutis107@gmail.com>
2016-06-16 22:23:00 +0900

    use QWidget, layout

--- calculator.py
+++ calculator.py
@@ -8,87 +8,23 @@ class Example(QtGui.QMainWindow):
 
 #Creat all numbers step by step    
     def initUI(self):
-        """Button setting"""
-        self.btn0 = QtGui.QPushButton('0', self)
-        self.btn0.resize(50, 40)
-        self.btn0.move(70, 250)
+        #Button setting
+        grid = QtGui.QGridLayout()
+        self.setLayout(grid)
 
-        self.btn1 = QtGui.QPushButton('1', self)
-        self.btn1.resize(50, 40)
-        self.btn1.move(10, 200)
+        names = ['Cls', 'Bck', '', 'Close',
+                '7', '8', '9', '/',
+                '4', '5', '6', '*',
+                '1', '2', '3', '-',
+                '0', '.', '=', '+']
         
-        self.btn2 = QtGui.QPushButton('2', self)
-        self.btn2.resize(50, 40)
-        self.btn2.move(70, 200)
+        positions = [(i, j) for i in range(5) for j in range(4)]
 
-        self.btn3 = QtGui.QPushButton('3', self)
-        self.btn3.resize(50, 40)
-        self.btn3.move(130, 200)
-
-        self.btn4 = QtGui.QPushButton('4', self)
-        self.btn4.resize(50, 40)
-        self.btn4.move(10, 150)
-
-        self.btn5 = QtGui.QPushButton('5', self)
-        self.btn5.resize(50, 40)
-        self.btn5.move(70, 150)
-
-        self.btn6 = QtGui.QPushButton('6', self)
-        self.btn6.resize(50, 40)
-        self.btn6.move(130, 150)
-
-        self.btn7 = QtGui.QPushButton('7', self)
-        self.btn7.resize(50, 40)
-        self.btn7.move(10, 100)
-
-        self.btn8 = QtGui.QPushButton('8', self)
-        self.btn8.resize(50, 40)
-        self.btn8.move(70, 100)
-
-        self.btn9 = QtGui.QPushButton('9', self)
-        self.btn9.resize(50, 40)
-        self.btn9.move(130, 100)
-
-        self.btnCE = QtGui.QPushButton('CE', self)
-        self.btnCE.resize(50, 40)
-        self.btnCE.move(10, 50)
-
-        self.btnC = QtGui.QPushButton('C', self)
-        self.btnC.resize(50, 40)
-        self.btnC.move(70, 50)
-
-        self.btnBck = QtGui.QPushButton('<-', self)
-        self.btnBck.resize(50, 40)
-        self.btnBck.move(130, 50)
-
-        self.btnDiv = QtGui.QPushButton('/', self)
-        self.btnDiv.resize(50, 40)
-        self.btnDiv.move(190, 50)
-
-        self.btnMul = QtGui.QPushButton('*', self)
-        self.btnMul.resize(50, 40)
-        self.btnMul.move(190, 100)
-
-        self.btnSub = QtGui.QPushButton('-', self)
-        self.btnSub.resize(50, 40)
-        self.btnSub.move(190, 150)
-
-        self.btnAdd = QtGui.QPushButton('+', self)
-        self.btnAdd.resize(50, 40)
-        self.btnAdd.move(190, 200)
-
-        self.btnEqual = QtGui.QPushButton('=', self)
-        self.btnEqual.resize(50, 40)
-        self.btnEqual.move(190, 250)
-
-        self.btnSwitch = QtGui.QPushButton('+/-', self)
-        self.btnSwitch.resize(50, 40)
-        self.btnSwitch.move(10, 250)
-
-        self.btnDot = QtGui.QPushButton('.', self)
-        self.btnDot.resize(50, 40)
-        self.btnDot.move(130, 250)
-
+        for position, name in zip(positions, names):
+            if name == '':
+                continue
+            button = QtGui.QPushButton(name)
+            grid.addWidget(button, *position)
 
         # self.btn1.clicked.connect(self.btn1)
         # self.btn2.clicked.connect(self.btn2)
