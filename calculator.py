37e8e60ae5400874c84bb8ad9a42e0bde9bb4aa5
Seonghun Kang <lutis107@gmail.com>
2016-06-16 23:06:19 +0900

    find number in list using 'string.isdigit()'

--- calculator.py
+++ calculator.py
@@ -1,12 +1,11 @@
 import sys
 from PyQt4 import QtGui
 
-class Example(QtGui.QMainWindow):
+class Example(QtGui.QWidget):
     def __init__(self):
         super(Example, self).__init__()
         self.initUI()
 
-#Creat all numbers step by step    
     def initUI(self):
         #Button setting
         grid = QtGui.QGridLayout()
@@ -16,7 +15,7 @@ class Example(QtGui.QMainWindow):
                 '7', '8', '9', '/',
                 '4', '5', '6', '*',
                 '1', '2', '3', '-',
-                '0', '.', '=', '+']
+                '.', '0', '=', '+']
         
         positions = [(i, j) for i in range(5) for j in range(4)]
 
@@ -26,19 +25,25 @@ class Example(QtGui.QMainWindow):
             button = QtGui.QPushButton(name)
             grid.addWidget(button, *position)
 
+            if name.isdigit() == True:
+                button.clicked.connect(self.btnClicked)
+        
+        self.line = QtGui.QLineEdit()
+
+        grid.addWidget(self.line, 5, 0)
+
         # self.btn1.clicked.connect(self.btn1)
         # self.btn2.clicked.connect(self.btn2)
         # self.btn3.clicked.connect(self.plus)
         # self.btn4.clicked.connect(self.equal)
 
-        self.le = QtGui.QLineEdit(self)
-        self.le.setReadOnly(True)
-        self.le.resize(230, 30)
-        self.le.move(10, 10)
-
-        self.setGeometry(300, 300, 250, 300)
+        self.move(300, 150)
         self.setWindowTitle('Calculator')
         self.show()
+
+    def btnClicked(self):
+        sender = self.sender()
+        self.line.setText(sender.text())
 
 def main():
     app = QtGui.QApplication(sys.argv)
